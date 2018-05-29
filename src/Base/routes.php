9e48262363f94af06e8f3d9ac3b4a0dcdbb6fb6e
xandros15 <xandros92@gmail.com>
2018-05-29 20:04:53 +0200

    add admin frontend

--- src/Base/routes.php
+++ src/Base/routes.php
@@ -23,4 +23,28 @@ $app->group('/admin', function () {
 
         return $response;
     });
+
+    $app->group('/songs', function () {
+        /** @var $app App */
+        $app = $this;
+        $app->get('', function (Request $request, Response $response) {
+            $this->logger->info("'/admin/songs' route");
+
+            return $this->view->render($response, 'admin.twig', ['songs' => $this->songs->find()]);
+        });
+
+        $app->get('/add', function (Request $request, Response $response) {
+            $this->logger->info("'/admin/songs/add' route");
+
+            return $this->view->render($response, 'songs/_form.twig');
+        })->setName('song.add');
+
+        $app->get('/edit/{id}', function (Request $request, Response $response) {
+            $id = $request->getAttribute('id', '');
+            $this->logger->info("'/admin/songs/edit/{$id}' route");
+            $song = $this->songs->read($id);
+
+            return $this->view->render($response, 'songs/_form.twig', ['song' => $song]);
+        })->setName('song.edit');
+    });
 })->add(new AdminAuthMiddleware($app->getContainer()));
